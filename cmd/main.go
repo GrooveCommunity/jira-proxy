4d3d3dd609c5fcf49dc82266fdbce12a54e86996
Eduardo Dicarte <eduardo.pereira@groove.tech>
2021-06-09 16:10:31 -0300

    Validação de webhook

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"os"
+
+	"github.com/GrooveCommunity/proxy-jira/entity"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/proxy-jira/healthy", handleValidateHealthy).Methods("GET")
+	router.HandleFunc("/proxy-jira/webhook", handleWebhook).Methods("GET")
+
+	log.Println("Port: ", os.Getenv("APP_PORT"))
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("APP_PORT"), router))
+}
+
+func handleValidateHealthy(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(entity.Healthy{Status: "Success!"})
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	log.Println(params)
+}
