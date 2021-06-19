a20e77dd3290187d33925f85c4b31b8f49ee51a8
Eduardo Dicarte <eduardo.pereira@groove.tech>
2021-06-19 14:38:22 -0300

    Ignorando fila de metrics

--- internal/forward.go
+++ internal/forward.go
@@ -65,7 +65,7 @@ func ForwardIssue(jiraRequest entity.JiraRequest, body []byte, projectID, topicD
 	}
 
 	go PublicMessage(projectID, topicDispatcher, payload)
-	go PublicMessage(projectID, topicMetrics, payload)
+	//go PublicMessage(projectID, topicMetrics, payload)
 }
 
 func unmarchallMap(dataMap map[string]interface{}) []glibentity.JiraCustomField {
