1ec01e18abb454413b4ce53da094e8bc9f95dce5
Eduardo Dicarte <eduardo.pereira@groove.tech>
2021-06-22 20:54:33 -0300

    Correção de sintaxe na validação das variáveis de ambiente

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func main() {
 	topicDispatcher = os.Getenv("TOPIC_ID_DISPATCHER")
 	topicMetrics = os.Getenv("TOPIC_ID_METRICS")
 
-	if projectID == "" || topicDispatcher == "" || topicMetrics == "" || os.Getenv("TOKEN_DISPATCHER_PAYGO_DISCORD") || os.Getenv("CHANNEL_ID_DISCORD") {
+	if projectID == "" || topicDispatcher == "" || topicMetrics == "" || os.Getenv("TOKEN_DISPATCHER_PAYGO_DISCORD") == "" || os.Getenv("CHANNEL_ID_DISCORD") == "" {
 		log.Fatal("Nem todas as variáveis de ambiente requeridas foram fornecidas. ")
 	}
 
