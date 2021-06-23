7283f69cab4c43b7fadc8ce7f11b445b6f1808d9
Eduardo Dicarte <eduardo.pereira@groove.tech>
2021-06-22 23:05:15 -0300

    Validação no envio da mensagem

--- internal/forward.go
+++ internal/forward.go
@@ -63,7 +63,7 @@ func ForwardIssue(jiraRequest entity.JiraRequest, body []byte, projectID, topicD
 
 func validateIssueDispatcher(jiraRequest entity.JiraRequest, projectID, topicName string, payload []byte) {
 	if (jiraRequest.EventName == "jira:issue_created" || jiraRequest.EventName == "jira:issue_updated") && jiraRequest.Issue.Fields.Status.Name == "Aguardando SD" {
-		SendMessageToChannel("\nTicket ID: " + jiraRequest.Issue.ID + "\nTicket Key:" + jiraRequest.Issue.Key + "\nEvent: " + jiraRequest.EventName)
+		SendMessageToChannel("\nTicket ID: " + jiraRequest.Issue.ID + "\nTicket Key:" + jiraRequest.Issue.Key + "\nEvent: " + jiraRequest.EventName + "\nPriority: " + jiraRequest.Issue.Fields.Priority.Name + "\n\nSLA: " + getSLA(jiraRequest.Issue.Fields.Priority.Name) + "\n\n")
 		/*"Priority: " + jiraRequest.Issue.Fields.Priority.Name + "\n\n" +
 		"SLA: " + getSLA(jiraRequest.Issue.Fields.Priority.Name) + "\n")*/
 	}
