4e8aca5a4b7d5de2d9d1ba7efc95bbac97cc65d6
Eduardo Dicarte <eduardo.pereira@groove.tech>
2021-06-22 22:56:51 -0300

    Validação no envio da mensagem

--- internal/forward.go
+++ internal/forward.go
@@ -63,11 +63,9 @@ func ForwardIssue(jiraRequest entity.JiraRequest, body []byte, projectID, topicD
 
 func validateIssueDispatcher(jiraRequest entity.JiraRequest, projectID, topicName string, payload []byte) {
 	if (jiraRequest.EventName == "jira:issue_created" || jiraRequest.EventName == "jira:issue_updated") && jiraRequest.Issue.Fields.Status.Name == "Aguardando SD" {
-		SendMessageToChannel(
-			"===============================================================================================================\n" +
-				"Ticket ID: " + jiraRequest.Issue.ID + "\nTicket Key:" + jiraRequest.Issue.Key + "\n " +
-				"Priority: " + jiraRequest.Issue.Fields.Priority.Name + "\n\n" +
-				"SLA: " + getSLA(jiraRequest.Issue.Fields.Priority.Name) + "\n")
+		SendMessageToChannel("\nTicket ID: " + jiraRequest.Issue.ID + "\nTicket Key:" + jiraRequest.Issue.Key + "\nEvent: " + jiraRequest.EventName)
+		/*"Priority: " + jiraRequest.Issue.Fields.Priority.Name + "\n\n" +
+		"SLA: " + getSLA(jiraRequest.Issue.Fields.Priority.Name) + "\n")*/
 	}
 
 	for _, item := range jiraRequest.Issue.Fields.CustomFields {
